3ebeabfb9801b7fccec63ff42c1af3f157b347c6
Jason <jason13201@outlook.com>
2021-03-14 15:42:58 +0530

    Updated guild members

--- website/bot.py
+++ website/bot.py
@@ -1,14 +1,20 @@
 import discord
 from config import BotToken
 
+intents = discord.Intents(messages=True, guilds=True, members=True)
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print("Logged on as {0}!".format(self.user))
 
+        for guild in client.guilds:
+            async for member in guild.fetch_members():
+                print(member)
+
     async def on_message(self, message):
         print("Message from {0.author}: {0.content}".format(message))
 
 
-client = MyClient()
+client = MyClient(intents=intents)
 client.run(BotToken)
